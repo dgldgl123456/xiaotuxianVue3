e2a0e3d6f4dff0318a7c001d2c498bbaa195b55e
dgldgl <1035377903@qq.com>
2025-10-25 14:11:56 +0800

    element主题色的定制

--- vue3-rabbit/src/main.js
+++ vue3-rabbit/src/main.js
@@ -6,18 +6,18 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-// --- 1. 添加 Element Plus 的导入 ---
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
-// ------------------------------------
+// // --- 1. 添加 Element Plus 的导入 ---
+// import ElementPlus from "element-plus";
+// import "element-plus/dist/index.css";
+// // ------------------------------------
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 
-// --- 2. 在挂载前全局使用 Element Plus ---
-app.use(ElementPlus);
-// ------------------------------------
+// // --- 2. 在挂载前全局使用 Element Plus ---
+// app.use(ElementPlus);
+// // ------------------------------------
 
 app.mount("#app");
--- vue3-rabbit/vite.config.js
+++ vue3-rabbit/vite.config.js
@@ -3,26 +3,47 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
-// 下面的这三行可以删掉，也可以留着，因为不再使用了
-// import AutoImport from "unplugin-auto-import/vite";
-// import Components from "unplugin-vue-components/vite";
-// import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+
+// 1. 引入实现按需导入的插件
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    // AutoImport({  // <--- 注释掉这一整块
-    //   resolvers: [ElementPlusResolver()],
-    // }),
-    // Components({  // <--- 注释掉这一整块
-    //   resolvers: [ElementPlusResolver()],
-    // }),
+
+    // 2. 配置 unplugin-auto-import 插件
+    AutoImport({
+      // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox
+      resolvers: [ElementPlusResolver()],
+    }),
+
+    // 3. 配置 unplugin-vue-components 插件
+    Components({
+      // 自动导入 Element Plus 组件
+      resolvers: [
+        // 配置 Element Plus 组件库的解析器，并指定样式导入方式为 sass
+        ElementPlusResolver({ importStyle: "sass" }),
+      ],
+    }),
   ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 4. 自动导入定制化样式文件，进行样式覆盖
+        // additionalData 的值会被注入到每一个 scss 文件的开头
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      },
     },
   },
 });
