fe16e990e4b3d7dbd126f55247926eb76bcf04ae
dgldgl <1035377903@qq.com>
2025-10-25 13:38:45 +0800

    elementplus

--- vue3-rabbit/src/main.js
+++ vue3-rabbit/src/main.js
@@ -1,14 +1,23 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+// --- 1. 添加 Element Plus 的导入 ---
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+// ------------------------------------
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+
+// --- 2. 在挂载前全局使用 Element Plus ---
+app.use(ElementPlus);
+// ------------------------------------
+
+app.mount("#app");
--- vue3-rabbit/vite.config.js
+++ vue3-rabbit/vite.config.js
@@ -1,18 +1,28 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+// 下面的这三行可以删掉，也可以留着，因为不再使用了
+// import AutoImport from "unplugin-auto-import/vite";
+// import Components from "unplugin-vue-components/vite";
+// import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    // AutoImport({  // <--- 注释掉这一整块
+    //   resolvers: [ElementPlusResolver()],
+    // }),
+    // Components({  // <--- 注释掉这一整块
+    //   resolvers: [ElementPlusResolver()],
+    // }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
