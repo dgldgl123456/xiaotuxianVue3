3fa480d6fe521ccad73450e8a532b8dbe1801c52
dgldgl <1035377903@qq.com>
2025-11-03 17:42:28 +0800

    静态资源创建

--- vue3-rabbit/src/App.vue
+++ vue3-rabbit/src/App.vue
@@ -1,7 +1,8 @@
 <script setup></script>
 
 <template>
-  <el-button type="primary">主要按钮</el-button>
+  <!-- 一级路由出口组件 -->
+  <RouterView />
 </template>
 
 <style scoped>
--- vue3-rabbit/src/main.js
+++ vue3-rabbit/src/main.js
@@ -1,16 +1,11 @@
-import "./assets/main.css";
-
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
 
-// // --- 1. 添加 Element Plus 的导入 ---
-// import ElementPlus from "element-plus";
-// import "element-plus/dist/index.css";
-// // ------------------------------------
-//测试接口函数
+import "@/styles/common.scss";
+
 import { getCategory } from "@/apis/testAPI";
 getCategory().then((res) => {
   console.log(res);
@@ -19,9 +14,5 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
-// // --- 2. 在挂载前全局使用 Element Plus ---
-// app.use(ElementPlus);
-// // ------------------------------------
 
 app.mount("#app");
